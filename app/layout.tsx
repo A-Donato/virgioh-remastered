246c0ecea76e2fcda3d0c861d6ae51f27b672874
Alexis <alexis.donato95@gmail.com>
2024-02-12 15:28:37 -0300

    feat: add speedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import { Providers } from "./providers";
 import { robotoFont } from "./ui/fonts";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export default function RootLayout({
   children,
 }: {
@@ -12,6 +14,7 @@ export default function RootLayout({
       <body className={`${robotoFont} antialiased`}>
         <Providers>{children}</Providers>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
